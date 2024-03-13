1e1c5b6d2bdff03470abd17695003137344921ac
Budi Gunawan <budiindra.g@gmail.com>
2024-03-13 11:06:03 +0700

    feat: init index.html and index.js contents

---
+++ index.js
@@ -0,0 +1,29 @@
+const contacs = [
+  {
+    id: 1,
+    fullName: 'Budi Indra Gunawan',
+    email: 'budigunawan@mail.com',
+    phoneNumber: '+62812 3456 7890',
+    labels: ['Family'],
+    birthday: '31/12/2000',
+  },
+  {
+    id: 2,
+    fullName: 'John Doe',
+    email: 'johndoe@mail.com',
+    phoneNumber: '+62857 1234 5678',
+    labels: ['Friend'],
+    birthday: '31/12/2001',
+  },
+  {
+    id: 1,
+    fullName: 'Jane Doe',
+    email: 'janedoe@mail.com',
+    phoneNumber: '+62898 7654 3210',
+    labels: ['Work'],
+    birthday: '31/12/1999',
+  },
+];
+
+console.log(contacs, 'this is contacts');
+console.log(contacs[0], 'first index of contacts');
