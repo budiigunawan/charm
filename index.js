befdf28d99477076fda86b199b185829cb75bdcb
Budi Gunawan <budiindra.g@gmail.com>
2024-03-26 20:06:54 +0700

    feat: add async code example

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 let contacts = [
   {
     id: 1,
-    fullName: 'Budi Indra Gunawan',
-    email: 'budigunawan@mail.com',
-    phoneNumber: '6281234567890',
-    labels: ['Family'],
-    birthday: '1996-07-31T03:24:00',
-    avatar: 'https://i.pravatar.cc/150?img=53',
-    notes: '',
+    fullName: "Budi Indra Gunawan",
+    email: "budigunawan@mail.com",
+    phoneNumber: "6281234567890",
+    labels: ["Family"],
+    birthday: "1996-07-31T03:24:00",
+    avatar: "https://i.pravatar.cc/150?img=53",
+    notes: "",
   },
   {
     id: 2,
-    fullName: 'John Doe',
-    email: 'johndoe@mail.com',
-    phoneNumber: '6285712345678',
-    labels: ['Friend'],
-    birthday: '1995-12-17T03:24:00',
-    avatar: 'https://i.pravatar.cc/150?img=56',
-    notes: '',
+    fullName: "John Doe",
+    email: "johndoe@mail.com",
+    phoneNumber: "6285712345678",
+    labels: ["Friend"],
+    birthday: "1995-12-17T03:24:00",
+    avatar: "https://i.pravatar.cc/150?img=56",
+    notes: "",
   },
   {
     id: 3,
-    fullName: 'Jane Doe',
-    email: 'janedoe@mail.com',
-    phoneNumber: '6289876543210',
-    labels: ['Work'],
-    birthday: '1995-12-17T03:24:00',
-    avatar: 'https://i.pravatar.cc/150?img=45',
-    notes: '',
+    fullName: "Jane Doe",
+    email: "janedoe@mail.com",
+    phoneNumber: "6289876543210",
+    labels: ["Work"],
+    birthday: "1995-12-17T03:24:00",
+    avatar: "https://i.pravatar.cc/150?img=45",
+    notes: "",
   },
 ];
 
@@ -56,12 +56,12 @@ function renderContacts() {
   for (let index = 0; index < contacts.length; index++) {
     const contact = contacts[index];
     const age = getAge(contact.birthday);
-    const ageCategory = age > 30 ? 'is old enough' : 'is still young';
+    const ageCategory = age > 30 ? "is old enough" : "is still young";
     console.log(
       `${contact.id}. ${contact.fullName} (${contact.phoneNumber}) ${ageCategory} `
     );
   }
-  console.log('');
+  console.log("");
 }
 
 function editContact(contactId, payload) {
@@ -79,30 +79,30 @@ function searchContact(keyword) {
   const result = contacts.filter((contact) =>
     contact.fullName.toLowerCase().includes(keyword.toLowerCase())
   );
-  console.log(result, 'search result');
+  console.log(result, "search result");
 }
 
 addContact({
-  fullName: 'Dita Lestari',
-  email: 'ditalestari@mail.com',
-  phoneNumber: '6281234567890',
-  labels: ['Family'],
-  birthday: '1996-03-18T03:24:00',
-  avatar: 'https://i.pravatar.cc/150?img=53',
-  notes: '',
+  fullName: "Dita Lestari",
+  email: "ditalestari@mail.com",
+  phoneNumber: "6281234567890",
+  labels: ["Family"],
+  birthday: "1996-03-18T03:24:00",
+  avatar: "https://i.pravatar.cc/150?img=53",
+  notes: "",
 });
 
 editContact(1, {
-  fullName: 'Budi I Gunawan',
-  email: 'budi.igunawan@mail.com',
-  phoneNumber: '6281234567890',
-  labels: ['Family'],
-  birthday: '1996-07-31T03:24:00',
-  avatar: 'https://i.pravatar.cc/150?img=53',
-  notes: '',
+  fullName: "Budi I Gunawan",
+  email: "budi.igunawan@mail.com",
+  phoneNumber: "6281234567890",
+  labels: ["Family"],
+  birthday: "1996-07-31T03:24:00",
+  avatar: "https://i.pravatar.cc/150?img=53",
+  notes: "",
 });
 
-searchContact('doe');
+searchContact("doe");
 
 deleteContact(2);
 
